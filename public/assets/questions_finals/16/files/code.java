ce0774d6b8057904461a93b365edc089c6d0a097
Diptangsu Goswami <deep.dippu@gmail.com>
2018-04-17 16:25:06 +0530

    Add files via upload

---
+++ public/assets/questions_finals/16/files/code.java
@@ -0,0 +1,67 @@
+public class code {	 	 	  
+	
+    public static	void	main(String[] args)	{   
+	
+     System.out.println("You	will	find the flag	when you	run");
+	
+     System.out.print("this program\n");	     
+	
+     System.out.println(obfuscate());		  		 
+	
+    } 		 		  
+	
+    public static	String	obfuscate() {   	
+	
+     int	i='w'	, j='s' ,	k='p'	,	l='a';
+	
+     i=i^7; 	     
+	
+     j=j>>1;		 	  	
+	
+     l=l<<1;			  		
+	
+     i-=2; 			 	 
+	
+     j+=54; 	     
+	
+     l-=93;			 	  
+	
+     char[]	a={(char)i,(char)j,(char)k,(char)l};		 			
+	
+     String	b=new	String(a);return b;				
+	
+    } 	 					
+	
+}     			    
+	
+     			  	 
+	
+     		 				
+	
+     		  			
+	
+     			  	 
+	
+     		    	
+	
+     		 		 	
+	
+     			  		
+	
+     	 					
+	
+     		 	  	
+	
+     		 			 
+	
+     	 					
+	
+     		 				
+	
+     		 			 
+	
+     		  	 	
+	
+  
+
+
